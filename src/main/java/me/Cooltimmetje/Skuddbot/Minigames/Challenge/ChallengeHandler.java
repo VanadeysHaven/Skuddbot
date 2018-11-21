8e4dd0957669168c85f4a54ed3256bc30b1a1b8d
Tim Medema <Cooltimmetje@me.com>
2018-11-21 19:39:13 +0100

    another small tweak

--- src/main/java/me/Cooltimmetje/Skuddbot/Minigames/Challenge/ChallengeHandler.java
+++ src/main/java/me/Cooltimmetje/Skuddbot/Minigames/Challenge/ChallengeHandler.java
@@ -169,7 +169,7 @@ public class ChallengeHandler {
         if(suWinner.getChallengeStreak() > 1) {
             streakString = (suWinner.getChallengeStreak() == 2 ? "**Win streak started:** 2 wins" : "**Win streak continued:** " + suWinner.getChallengeStreak() + " wins") + " (+" + bonusXP + " bonus " + EmojiHelper.getEmoji("xp_icon") + ")";
         }
-        if(suWinner.getChallengeStreak() > suWinner.getChallengeLongestStreak()){
+        if(suWinner.getChallengeStreak() > suWinner.getChallengeLongestStreak() && suWinner.getChallengeStreak() > 2){
             streakString = streakString + " - **New longest winstreak:** *" + suWinner.getChallengeStreak() + " wins*";
         }
         final String finalStreakString = streakString;
