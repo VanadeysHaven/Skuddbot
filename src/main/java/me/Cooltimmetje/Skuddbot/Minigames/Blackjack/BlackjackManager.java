24628931aa877e09a225df0256445eef205cf287
Cooltimmetje <Cooltimmetje@me.com>
2019-02-14 10:53:49 +0100

    I think I've made my point

--- src/main/java/me/Cooltimmetje/Skuddbot/Minigames/Blackjack/BlackjackManager.java
+++ src/main/java/me/Cooltimmetje/Skuddbot/Minigames/Blackjack/BlackjackManager.java
@@ -10,16 +10,12 @@ import sx.blah.discord.handle.obj.IMessage;
  * This handles all games of blackjack on a global basis.
  *
  * @author Tim (Cooltimmetje)
- * @version v0.4.51-ALPHA
+ * @version v0.4.61-ALPHA
  * @since v0.4.5-ALPHA
  */
 public class BlackjackManager {
 
     public static void run(IMessage message){
-        if(message.getGuild().getLongID() == 198483566026424321L && message.getContent().split(" ")[0].equalsIgnoreCase("!bj")){
-            return;
-        }
-
         ServerManager.getServer(message.getGuild().getStringID()).getBlackjackHandler().startNewGame(message.getAuthor(), message);
     }
 
